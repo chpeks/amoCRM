c49313fac5a0909f9e163d28dff8b6b2583fa852
Maksim <67479157+chpeks@users.noreply.github.com>
2021-09-03 15:36:52 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const limit = 25;
+let page = 1;
+let getContactsListQueryUrl = '/api/v4/contacts';
+let conditionalTaskCreation = '/api/v4/tasks';
+let text = "Контакт без сделок";
+let complete_till = new Date(2021,9,10);
+
+function getContacts() {
+    $.ajax({
+        url: getContactsListQueryUrl,
+        method: 'GET',
+        data: {
+            limit: limit,
+            with: 'leads',
+            page: page
+        }
+    }).done(function(data) {
+        for (i = 0; data['contacts']['leads'] === ""; i++) {
+            createTasks();
+        }
+
+
+        if (!!data['concat']['leads']) {
+            console.log(data)
+
+        } else {
+            console.log('Контактов нет');
+            return false;
+        }
+    }).fail(function(data) {
+        console.log('Что-то пошло не так c получением контактов');
+        console.log(data);
+        return false;
+    })
+
+
+    page++;
+}
+function createTasks() {
+    $.ajax({
+        url: conditionalTaskCreation,
+        method: 'POST',
+        record: {
+            text: text,
+            complete_till: complete_till,
+
+        }
+    })
+}
+
+getContacts();
